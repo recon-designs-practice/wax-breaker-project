23cfe55905cec86fd86b3ae5a4611a6813b2c446
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-10-14 02:27:40 -0500

    fix(Button): replaced old button with component library button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
+import React, { useState } from 'react'
 import Button from "./components/button/Button"
+import { Input } from "be-ubiquitous"
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+
   return (
     <div>
       <h1>Hello world</h1>
+      <p>{inputValue}</p>
       <Button label="Labelssss" type="button"  onClick={() => alert('fired')}  />
+      {/** @ts-expect-error Type '(e: React.ChangeEvent<HTMLInputElement>) => void' is not (delete for full error */}
+      <Input label='First name' caption='Just the first name only' value={inputValue} onchange={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)} />
     </div>
   )
 }
