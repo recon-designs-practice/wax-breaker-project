b95706378d714f2eb942eba17740c70793363b46
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-10-21 02:17:21 -0500

    feat: added basic routing and pages

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,41 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+// import App from "./App"
+import Root from "./routes/root"
+import ErrorPage from "./error-page"
+import Signin from "./pages/Signin"
+import Dashboard from "./pages/Dashboard"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <Signin message='This is the sign in page.' />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard message="This is the dashboard page." />
+      }
+    ]
+  },
+  // {
+  //   path: '/dashboard',
+  //   element: <Dashboard message="This is the dashboard page." />
+  // }
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
