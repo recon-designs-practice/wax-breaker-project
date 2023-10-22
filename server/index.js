4b9dadc7ce0df301466871befee0071fd7bf8d3d
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-10-22 02:25:51 -0500

    feat: added last of the routes

--- server/index.js
+++ server/index.js
@@ -10,5 +10,7 @@ app.use(express.json())
 
 app.get('/box_breaks', routes.getAllBreaks)
 app.post('/box_breaks', routes.createNewBreak)
+app.get('/box_breaks/:id', routes.getBreakById)
+app.delete('/box_breaks/:id', routes.deleteBreakById)
 
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}.`))
--- server/routes.js
+++ server/routes.js
@@ -13,8 +13,8 @@ const getAllBreaks = async (req, res) => {
 
 const createNewBreak = async (req, res) => {
   try {
-    const breakDate = moment().format()
     const { break_name } = req.body
+    const breakDate = moment().format()
 
     const newBoxBreak = await pool.query(
       "INSERT INTO box_break (break_name, break_date) VALUES($1, $2) RETURNING *;",
@@ -27,7 +27,39 @@ const createNewBreak = async (req, res) => {
   }
 }
 
+const getBreakById = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const selectedBreak = await pool.query(
+      'SELECT * FROM box_break WHERE box_break_id = $1;',
+      [id]
+    )
+
+    res.json(selectedBreak.rows[0])
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+const deleteBreakById = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const deletedBreak = await pool.query(
+      'DELETE FROM box_break WHERE box_break_id = $1 RETURNING *;',
+      [id]
+    )
+
+    res.json(`Box break '${deletedBreak.rows[0].break_name.toUpperCase()}' has been deleted.`)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
 module.exports = {
   getAllBreaks,
-  createNewBreak
+  createNewBreak,
+  getBreakById,
+  deleteBreakById
 }
