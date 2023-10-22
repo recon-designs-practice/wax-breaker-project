59876e807cbd9807e4da0335eb6bf89f05bcaa2a
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-10-22 15:11:13 -0500

    feat: added more user routes to backend

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const cors = require('cors')
-const routes = require('./routes/routes')
+const breaksRoutes = require('./routes/breaksRoutes')
+const usersRoutes = require('./routes/userRoutes')
 
 const app = express()
 const PORT = 5656
 
+// Middleware
 app.use(cors())
 app.use(express.json())
 
-app.get('/box_breaks', routes.getAllBreaks)
-app.post('/box_breaks', routes.createNewBreak)
-app.get('/box_breaks/:id', routes.getBreakById)
-app.delete('/box_breaks/:id', routes.deleteBreakById)
+// breaks routes
+app.get('/box_breaks', breaksRoutes.getAllBreaks)
+app.post('/box_breaks', breaksRoutes.createNewBreak)
+app.get('/box_breaks/:id', breaksRoutes.getBreakById)
+app.delete('/box_breaks/:id', breaksRoutes.deleteBreakById)
+
+// users routes
+app.get('/users', usersRoutes.getAllUsers)
+app.post('/users', usersRoutes.createNewUser)
+app.delete('/users/:id', usersRoutes.deleteUserById)
 
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}.`))
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,39 @@
+const pool = require('../db')
+
+const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await pool.query('SELECT * FROM users;')
+
+    res.json(allUsers.rows)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+const createNewUser = async (req, res) => {
+  const { first_name, last_name } = req.body
+
+  const newUser = await pool.query(
+    'INSERT INTO users (first_name, last_name) VALUES($1, $2) RETURNING *;',
+    [first_name, last_name]
+  )
+
+  res.json(newUser.rows[0])
+}
+
+const deleteUserById = async (req, res) => {
+  const { id } = req.params
+
+  const deletedUser = await pool.query(
+    'DELETE FROM users WHERE user_id = $1 RETURNING *;',
+    [id]
+  )
+
+  res.json(`User '${deletedUser.rows[0].first_name.toUpperCase() + " " + deletedUser.rows[0].last_name.toUpperCase()}' has been deleted.`)
+}
+
+module.exports = {
+  getAllUsers,
+  createNewUser,
+  deleteUserById
+}
