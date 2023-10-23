56c8bc8b0751b003c8e6d24c439b4a00c4a5f301
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-10-23 08:52:15 -0500

    feat: added Card component and rendering each boxbreak with one

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import axios from "axios"
+import Card from "../components/card/Card"
 
 const api = "http://localhost:5656/box_breaks"
 
@@ -24,11 +25,30 @@ const Dashboard = () => {
   }, [])
 
   return (
-    <div>
+    <div style={{ paddingLeft: '40px', paddingRight: '40px' }}>
       <h1>Dashboard</h1>
-      {listOfBreaks.map((boxBreak: any, idx: number) => {
-        return <p key={idx}>{boxBreak.break_name}</p>
-      })}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(12, 1fr)",
+          gap: '20px'
+        }}
+      >
+        {listOfBreaks.map((boxBreak: any, idx: number) => {
+          console.log(boxBreak)
+          return (
+            <Card
+              key={idx}
+              style={{
+                gridColumn: "span 3",
+              }}
+            >
+              <h3>{boxBreak.break_name}</h3>
+              <p>{boxBreak.break_date}</p>
+            </Card>
+          )
+        })}
+      </div>
     </div>
   )
 }
