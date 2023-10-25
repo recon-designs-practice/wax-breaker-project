d15b0b8340fd4919f713a97afe855a4d647bfdd8
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-10-24 22:34:01 -0500

    feat: added delete button and delete api call

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,15 @@
 import React from "react"
 import axios from "axios"
+import styled from "@emotion/styled"
 import Card from "../components/card/Card"
-import moment from 'moment'
+import { Button } from "be-ubiquitous"
+import moment from "moment"
 
 const api = "http://localhost:5656/box_breaks"
+
+const StyledButton = styled(Button)`
+  width: 100%;
+`
 
 const Dashboard = () => {
   const [listOfBreaks, setListOfBreaks] = React.useState([])
@@ -25,18 +31,46 @@ const Dashboard = () => {
       })
   }, [])
 
+  const deleteBreak = (breakId: any) => {
+    axios
+      .delete(`http://localhost:5656/box_breaks/${breakId}`)
+      .then((response) => {
+        console.log(response.data)
+
+        axios
+        .get(api)
+        .then((response) => {
+          const tempArr: any = []
+  
+          response.data.forEach((entry: any) => {
+            tempArr.push(entry)
+          })
+  
+          setListOfBreaks(tempArr)
+        })
+        .catch((error) => {
+          console.log(222, error)
+        })
+      })
+      .catch((error) => {
+        console.log(222, error)
+      })
+  }
+
   return (
-    <div style={{ paddingLeft: '40px', paddingRight: '40px' }}>
+    <div style={{ paddingLeft: "40px", paddingRight: "40px" }}>
       <h1>Dashboard</h1>
       <div
         style={{
           display: "grid",
           gridTemplateColumns: "repeat(12, 1fr)",
-          gap: '20px'
+          gap: "20px",
         }}
       >
         {listOfBreaks.map((boxBreak: any, idx: number) => {
-          const breakDate = moment(boxBreak.break_date).format('MMMM Do YYYY, h:mm:ss')
+          const breakDate = moment(boxBreak.break_date).format(
+            "MMMM Do YYYY, h:mm:ss"
+          )
 
           return (
             <Card
@@ -47,6 +81,10 @@ const Dashboard = () => {
             >
               <h3>{boxBreak.break_name}</h3>
               <p>{breakDate}</p>
+              <StyledButton
+                label="Button"
+                onClick={() => deleteBreak(boxBreak.box_break_id)}
+              />
             </Card>
           )
         })}
