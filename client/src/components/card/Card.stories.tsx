a5441ec11bb871fd60c07bc8ac77eac727fa7fc5
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-10-24 21:53:28 -0500

    feat(Card): added noPadding prop and stories

--- client/src/components/card/Card.stories.tsx
+++ client/src/components/card/Card.stories.tsx
@@ -11,10 +11,32 @@ export const Default: StoryObj<typeof Card> = {
     children: (
       <>
         <div>
-          <h2 style={{ margin: '0px' }}>Card title</h2>
+          <h3 style={{ margin: "0px" }}>2013 Topps Finest</h3>
         </div>
         <div>
-          <p style={{ margin: '4px 0px 0px 0px' }}>Card body</p>
+          <p style={{ margin: "4px 0px 0px 0px" }}>Dec 23rd 2020, 12:00 am</p>
+        </div>
+      </>
+    ),
+  },
+}
+
+export const NoPadding: StoryObj<typeof Card> = {
+  ...CardTemplate,
+  args: {
+    noPadding: true,
+    children: (
+      <>
+        <div style={{ background: "#EB6E1F", height: "88px" }}></div>
+        <div>
+          <h3 style={{ margin: "0px", padding: "4px 8px" }}>
+            2013 Topps Finest
+          </h3>
+        </div>
+        <div>
+          <p style={{ margin: "4px 0px 0px 0px", padding: "2px 8px" }}>
+            Dec 23rd 2020, 12:00 am
+          </p>
         </div>
       </>
     ),
@@ -34,9 +56,11 @@ const meta: Meta<typeof Card> = {
             gap: "20px",
           }}
         >
-          <div style={{
-            gridColumn: 'span 3'
-          }}>
+          <div
+            style={{
+              gridColumn: "span 3",
+            }}
+          >
             <CardStory />
           </div>
         </div>
--- client/src/components/card/Card.tsx
+++ client/src/components/card/Card.tsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import styled from '@emotion/styled'
+import React from "react"
+import styled from "@emotion/styled"
 
 type Props = {
-  children?: React.ReactNode,
+  children?: React.ReactNode
   classname?: string
   style?: any
+  noPadding?: boolean
 }
 
-const CardContainer = styled.div<Props>`
-  padding: 12px;
-  border: 2px solid black;
-  border-radius: 3px;
-`
+const CardContainer = styled.div<Props>(
+  {
+    border: "2px solid black",
+    borderRadius: "2px",
+  },
+  ({ noPadding }) => ({
+    padding: noPadding ? "0px" : "12px",
+  })
+)
 
-export default function Card({ children, classname, style }: Props) {
+export default function Card({ children, classname, style, noPadding }: Props) {
   return (
-    <CardContainer className={classname} style={style}>
+    <CardContainer className={classname} style={style} noPadding={noPadding}>
       {children}
     </CardContainer>
   )
