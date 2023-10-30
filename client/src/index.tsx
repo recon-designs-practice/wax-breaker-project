e0bb1d779811421e1c72c60c34cec07ce42ab0f3
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-10-30 00:58:29 -0500

    feat: added Zustand and removed context

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 // @ts-expect-error Module '"@emotion/react"' has no exported member 'ThemeProvider'.
 import { ThemeProvider } from "@emotion/react"
-import { BreaksProvider } from "./contexts/BreaksContext"
 import Dashboard from "./pages/Dashboard"
 import ErrorPage from "./error-page"
 import Signin from "./pages/Signin"
@@ -18,17 +17,15 @@ const router = createBrowserRouter([
   {
     path: "/sign-in",
     element: <Signin message="This is the sign in page." />,
-  }
+  },
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
-  <React.StrictMode>
-    <BreaksProvider>
-      <ThemeProvider theme={defaultTheme}>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </BreaksProvider>
-  </React.StrictMode>
+  <>
+    <ThemeProvider theme={defaultTheme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </>
 )
---
+++ client/src/stores/store.js
@@ -0,0 +1,11 @@
+import { createWithEqualityFn } from "zustand/traditional"
+
+const useBreaksStore = createWithEqualityFn((set) => ({
+  breaks: [],
+  setAllStoreBreaks: (stuffToAdd) => set(() => ({ breaks: [...stuffToAdd] })),
+  isNewBreakModalShowing: false,
+  setIsNewBreakModalShowing: () =>
+    set((state) => ({ isNewBreakModalShowing: !state.isNewBreakModalShowing })),
+}))
+
+export default useBreaksStore
