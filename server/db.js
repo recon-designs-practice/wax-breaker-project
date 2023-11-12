e363b9bc907a85b61de412065bc67b6468505d8b
Michael Stedman <michaelstedman81@gmail.com>
2023-11-12 17:32:00 -0600

    feat: reverted changes for render back

--- server/db.js
+++ server/db.js
@@ -1,18 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-  connectionString: 'dpg-cl8l4mavokcc73b142n0-a',
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-// const pool = new Pool({
-//   user: 'postgres',
-//   password: '122101',
-//   host: 'localhost',
-//   port: '5432',
-//   database: 'waxbreakerdb'
-// })
+  user: 'postgres',
+  password: '122101',
+  host: 'localhost',
+  port: '5432',
+  database: 'waxbreakerdb'
+})
 
 module.exports = pool
