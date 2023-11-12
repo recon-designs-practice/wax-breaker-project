453213035bbb17dba0d90b506d8f269b5517ad98
Michael Stedman <michaelstedman81@gmail.com>
2023-11-12 17:26:27 -0600

    changed db stuff for render

--- server/db.js
+++ server/db.js
@@ -1,11 +1,18 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-  user: 'postgres',
-  password: '122101',
-  host: 'localhost',
-  port: '5432',
-  database: 'waxbreakerdb'
-})
+  connectionString: 'dpg-cl8l4mavokcc73b142n0-a',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: '122101',
+//   host: 'localhost',
+//   port: '5432',
+//   database: 'waxbreakerdb'
+// })
 
 module.exports = pool
--- server/index.js
+++ server/index.js
@@ -11,16 +11,16 @@ app.use(cors())
 app.use(express.json())
 
 // breaks routes
-app.get('/box_breaks', breaksRoutes.getAllBreaks)
-app.post('/box_breaks', breaksRoutes.createNewBreak)
-app.get('/box_breaks/:id', breaksRoutes.getBreakById)
-app.put('/box_breaks/:id', breaksRoutes.updateBreakById)
-app.delete('/box_breaks/:id', breaksRoutes.deleteBreakById)
+// app.get('/box_breaks', breaksRoutes.getAllBreaks)
+// app.post('/box_breaks', breaksRoutes.createNewBreak)
+// app.get('/box_breaks/:id', breaksRoutes.getBreakById)
+// app.put('/box_breaks/:id', breaksRoutes.updateBreakById)
+// app.delete('/box_breaks/:id', breaksRoutes.deleteBreakById)
 
 // users routes
 app.get('/users', usersRoutes.getAllUsers)
-app.get('/users/:id', usersRoutes.getUserById)
+// app.get('/users/:id', usersRoutes.getUserById)
 app.post('/users', usersRoutes.createNewUser)
-app.delete('/users/:id', usersRoutes.deleteUserById)
+// app.delete('/users/:id', usersRoutes.deleteUserById)
 
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}.`))
